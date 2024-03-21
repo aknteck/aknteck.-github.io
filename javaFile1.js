b3dfd220e795ee6ada181f30b732003ffd66c5d3
aknteck <141295198+aknteck@users.noreply.github.com>
2024-03-21 15:31:32 -0400

    Add files via upload

---
+++ javaFile1.js
@@ -0,0 +1,13 @@
+let lastScrollpos = window.pageYOffset;
+let menuButtonCheck = document.querySelector(".menu-btn");
+window.onscroll = function() {
+	let currentScrollPos = window.pageYOffset;
+	if (lastScrollpos > currentScrollPos) {
+		document.getElementById("top-nav").style.top = "0";
+	} else {
+		document.getElementById("top-nav").style.top = "-75px";
+	}
+	lastScrollpos = currentScrollPos;
+	menuButtonCheck.checked = false;
+} 
+
